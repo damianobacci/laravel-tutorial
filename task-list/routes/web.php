87a6506218bf859477ae76050f5953c1a233a4a8
damianobacci <damiano.bacci@gmail.com>
2025-11-23 22:44:02 +0100

    feat: added different routes

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello';
+})->name('hello'); // named route
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello'); // redirect to named route
+}); 
+
+Route::get('/greet/{name}', function ($name) {
+    return "Hello, {$name}!"; // route with parameter
+});
+
+// php artisan route:list to get all routes
+
+Route::fallback(function () {
+    return 'The page you are looking for does not exist.'; // fallback route catch all unmatched routes
+});
